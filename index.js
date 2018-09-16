6bb269cbb381acd7f9c681c2efa2b3d090d8b702
Bill <jfg@163.com>
2018-09-16 18:06:34 +0800

    install log4js 3.0.5

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ global.dir = __dirname;
 const path = require('path');
 const Koa = require('koa');
 const static = require('koa-static');
+const log4js = require('log4js');
 
 const config = require('./config.js');
 const env = process.env.NODE_ENV || 'local';
 const cfg = config[env];
+
+const log = log4js.getLogger();
+log.level = 'debug';
+const resLog = async (ctx, next) => {
+	const start = Date.now();
+	await next();
+	const ms = Date.now() - start;
+	const bytes = ctx.response.header['content-length'] || 0;
+	log.info(`${ctx.method} ${ctx.url} ${ctx.response.status} ${bytes}bytes ${ms}ms`);
+}
 
 const normalizePort = val => (parseInt(val));
 const port = normalizePort(process.env.PORT || cfg.port || 3000);
 
 const router = require('./routes');
 const app = new Koa();
+app.use(resLog);
 app.use(static(path.join(global.dir, 'static')));
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(port);
 
-console.log('LazyPass is starting at port ' + port);
+log.info('LazyPass is starting at port ' + port);
