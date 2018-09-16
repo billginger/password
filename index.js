7d464ff45b0cdc85aed2cb0404691a136af1b696
Bill <jfg@163.com>
2018-09-16 17:56:17 +0800

    install koa-static 5.0.0

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ global.dir = __dirname;
 
 const path = require('path');
 const Koa = require('koa');
+const static = require('koa-static');
 
 const config = require('./config.js');
 const env = process.env.NODE_ENV || 'local';
@@ -12,6 +13,7 @@ const port = normalizePort(process.env.PORT || cfg.port || 3000);
 
 const router = require('./routes');
 const app = new Koa();
+app.use(static(path.join(global.dir, 'static')));
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(port);
 
